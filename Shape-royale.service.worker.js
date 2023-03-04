822fe14e9561a9bc2c81166893c54ff3fb7ce4c4
CaptainDeathead <115883954+CaptainDeathead@users.noreply.github.com>
2023-03-04 22:28:17 +0800

    added sounds

--- Shape-royale.service.worker.js
+++ Shape-royale.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1677852247|475510110";
+const CACHE_VERSION = "1677940051|88279081231";
 const CACHE_PREFIX = "Shape-Royale-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "Shape-royale.offline.html";
