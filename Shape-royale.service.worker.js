a80cbd1526d3a5eb32f08ebe3e887b8ab2ea001f
CaptainDeathead <115883954+CaptainDeathead@users.noreply.github.com>
2023-03-07 20:47:17 +0800

    bug fixes for update v1.0.0.5

--- Shape-royale.service.worker.js
+++ Shape-royale.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1678191791|4693027872";
+const CACHE_VERSION = "1678193187|6089374396";
 const CACHE_PREFIX = "Shape-Royale-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "Shape-royale.offline.html";
